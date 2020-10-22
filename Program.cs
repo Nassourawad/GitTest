71b4036d02215f73bc539f39b9bd95731d883b6f
Naser Awad <naser.awad585@gmail.com>
2020-10-22 14:26:55 +0200

    Remove hello, insert old

--- Program.cs
+++ Program.cs
@@ -10,8 +10,13 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+           // Console.WriteLine("Hello world!");
+           for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}^2 = {1}", i, i * i);
+                Console.ReadKey();
 
+            }
 
         }
     }
